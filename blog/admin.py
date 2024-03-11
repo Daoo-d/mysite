86788714952dbd9d39c80e1d0f3e869a34f1659d
Daoo-d <2020CS153@student.uet.edu.pk>
2024-03-11 23:28:40 +0500

    adding admin and tag model and rendering it

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Post,Author
+from .models import Post,Author,Tag
 
 class AdminPost(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
-    
+    list_filter = ("author",)
+
 
 # Register your models here.
 admin.site.register(Post,AdminPost)
 admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length = 50,null=True)
     last_name = models.CharField(max_length = 50,null=True)
@@ -17,7 +24,8 @@ class Post(models.Model):
     excerpt = models.TextField()
     content = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    slug = models.SlugField(default = "",null=False)
+    slug = models.SlugField(unique = True ,null=False)
+    tag = models.ManyToManyField(Tag, blank= True)
 
     def __str__(self) :
         return f"{self.title}  ({self.date})"
